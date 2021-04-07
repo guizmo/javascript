9962e5389cdf1f83bc4facc1c7cce5c6cc396bd7
Guillaume Bartolini <guillaumebartolini@gmail.com>
2021-04-07 15:02:37 +1100

    wip

--- students-list/app.js
+++ students-list/app.js
@@ -1,21 +1,22 @@
-console.log('students', JSON.stringify(students));
 const list = document.querySelector('.list');
 
-students.forEach((student) => {
-  const listItem = document.createElement('li');
-  const nameNode = createColumn('span', 'name', student.name);
-  const date = new Date(student.createdAt).toLocaleDateString('fr-FR');
-  const dateNode = createColumn('span', 'date', date);
-  const avatarNode = createColumn('img', 'avatar', null);
-  console.log(avatarNode);
-  nameNode.setAttribute('id', student.id);
-  avatarNode.setAttribute('src', student.avatar);
-  listItem.appendChild(avatarNode);
-  listItem.appendChild(nameNode);
-  listItem.appendChild(dateNode);
-  list.appendChild(listItem);
-});
+function showData(students) {
+  students.forEach((student) => {
+    const listItem = document.createElement('li');
+    const nameNode = createColumn('span', 'name', student.name);
+    const date = new Date(student.createdAt).toLocaleDateString('fr-FR');
+    const dateNode = createColumn('span', 'date', date);
+    const avatarNode = createColumn('img', 'avatar', null);
+    listItem.setAttribute('id', student.id);
+    avatarNode.setAttribute('src', 'avatar.png');
+    listItem.appendChild(avatarNode);
+    listItem.appendChild(nameNode);
+    listItem.appendChild(dateNode);
+    list.appendChild(listItem);
 
+    listItem.addEventListener('click', editStudent);
+  });
+}
 function createColumn(type, className, data) {
   const node = document.createElement(type);
   node.setAttribute('class', className);
@@ -30,11 +31,40 @@ function loadData() {
   xhttp.onreadystatechange = function () {
     console.log('readyState', this.readyState);
     if (this.readyState === 4 && this.status === 200) {
-      console.log(this.responseText);
+      const students = JSON.parse(this.responseText);
+      console.log(students);
+
+      showData(students);
     }
   };
-  xhttp.open('GET', 'students.json', true);
+  xhttp.open('GET', 'https://606ba946f8678400172e673d.mockapi.io/api/v1/students', true);
+  //xhttp.open('GET', 'students.json', true);
   xhttp.send();
 }
 
 loadData();
+
+function editStudent(e) {
+  const id = e.target.id;
+
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function () {
+    console.log('readyState', this.readyState);
+    if (this.readyState === 4 && this.status === 200) {
+      //const student = JSON.parse(this.responseText);
+      console.log('student', this.responseText);
+    }
+  };
+  xhttp.open('PUT', `https://606ba946f8678400172e673d.mockapi.io/api/v1/students/${id}`, true);
+  xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+  //xhttp.open('GET', 'students.json', true);
+  const newData = {
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/vladyn/128.jpg',
+    createdAt: '2021-04-06T14:26:30.758Z',
+    id,
+    name: 'Guillaume Bartolini'
+  };
+  xhttp.send(JSON.stringify(newData));
+}
